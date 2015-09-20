8ed8b81d49d5b10f0e7990cb3498c6ea72dacac8
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-19 20:43:48 -0700

    update to produce production values

--- inputdata/trades.R
+++ inputdata/trades.R
@@ -1,16 +1,11 @@
-args=commandArgs=TRUE
-traderIDfile=args[1] #file of user IDs
-tickerIDfile=args[2] #file of ticker names
-numtraders_persec=args[3] #number of traders making trades every second
+set.seed(1)
 
-#traderIDs = read.csv()
-traderIDs = seq(1,5)
-#tickerIDs = read.csv()
-tickerIDs = paste0("ti", seq(1,2))
-numtraders_persec=2
+traderIDs = seq(1,1000000)
+tickerIDs = c("MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DD", "XOM", "GE", "GS", "HD", "INTC", "IBM", "JNJ", "JPM", "MCD", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV" "UNH", "UTX", "VZ", "V", "WMT", "DIS")
+numtraders_persec=500000
 
 library(rkafka)
-kafkanode="ec2-54-67-10-231.us-west-1.compute.amazonaws.com:9092"
+kafkanode="ec2-54-215-247-116.us-west-1.compute.amazonaws.com:9092"
 producer=rkafka.createProducer(kafkanode)
 
 generate_trades = function() {
@@ -29,7 +24,6 @@ generate_trades = function() {
 
 while(1) {
 	generate_trades()
-	Sys.sleep(30)
 }
 
 rkafka.closeProducer(producer)
