7c0eb569e55530fa85c6cca43ee10b248e2c74e1
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-21 16:18:08 -0700

    bash script to create historical twitter dump

---
+++ inputdata/twitter_archive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+
+#Poll twitter to generate a historical set of tweets for later synthetic tweet generation
+
+python twitter_search.py
+for file in /home/ubuntu/synthetic_twitter/* ; do
+	sort -u $file > $file.tmp && mv $file.tmp $file
+done
+sleep(10m)
