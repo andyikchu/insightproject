53efeff3c43adcfd129f6fdb1788f2a97627a970
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-21 15:36:47 -0700

    first attempts to extract news from twitter

---
+++ inputdata/twitter_search.py
@@ -0,0 +1,28 @@
+from twython import Twython
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+
+#TODO: load from external file for all producers
+COMPANIES = ["MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DD", "XOM", "GE", "GS", "HD", "INTC", "IBM", "JNJ", "JPM", "MCD", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV", "UNH", "UTX", "VZ", "V", "WMT", "DIS"]
+
+COMPANIES = list(' #' + company + ' ' for company in COMPANIES)
+
+keyfile = open("/home/ubuntu/.twitterkeys/keys.txt", "r")
+TWITTERKEYS = dict(line.rstrip().split(": ") for line in keyfile)
+keyfile.close()
+
+t = Twython(TWITTERKEYS['consumer key'], TWITTERKEYS['consumer secret'], oauth_version=2)
+ACCESS_TOKEN = t.obtain_access_token()
+t = Twython(TWITTERKEYS['consumer key'], access_token=ACCESS_TOKEN)
+
+for co in COMPANIES:
+    dump = file.open("/home/ubuntu/synthetic_twitter/" + co + ".txt", "a")
+
+    results = t.search(q='#'+co, count=100)
+    for res in results:
+        text = res['text']
+        source = 'https://twitter.com/' + res['user']['name'] + '/status/' + res['id']
+
+        json = '{"company":"' + co + '", "summary":"' + text +'", "source":"' + source + '"}'
+        dump.write()
+    dump.close
---
+++ inputdata/twitter_stream.py
@@ -0,0 +1,24 @@
+#from twython import Twython
+from twython import TwythonStreamer
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+
+#TODO: load from external file for all producers
+COMPANIES = ["MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DD", "XOM", "GE", "GS", "HD", "INTC", "IBM", "JNJ", "JPM", "MCD", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV", "UNH", "UTX", "VZ", "V", "WMT", "DIS"]
+
+COMPANIES = list('#' + company for company in COMPANIES)
+
+keyfile = open("/home/ubuntu/.twitterkeys/keys.txt", "r")
+TWITTERKEYS = dict(line.rstrip().split(": ") for line in keyfile)
+keyfile.close()
+
+class NewsStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print data['text'].encode('utf-8') + data['timestamp_ms'] + str(data['id'])
+    def on_error(self, status_code, data):
+        print "error" + str(status_code)
+
+stream = NewsStreamer(TWITTERKEYS['consumer key'], TWITTERKEYS['consumer secret'], TWITTERKEYS['access token'], TWITTERKEYS['access token secret'])
+
+stream.statuses.filter(filter_level = 'medium', language='en', track=','.join(COMPANIES))
