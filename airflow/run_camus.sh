ab638e1fa77a45b374b76b7fd18f24ddc5b9af85
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-24 21:37:38 -0700

    move task into task subfolder

--- airflow/run_camus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-
-. ~/address.sh 
-ssh -i .ssh/insight-andy.pem ubuntu@$master_pubdns <<- 'ENDSSH'
-cd /usr/local/hadoop/etc/hadoop/
-hadoop jar /usr/local/hadoop/etc/hadoop/camus-example-0.1.0-SNAPSHOT-shaded.jar com.linkedin.camus.etl.kafka.CamusJob -P /usr/local/camus/camus-example/src/main/resources/camus.properties
-ENDSSH
