e653dbb88e2e547a7cc51ef841e783d288333035
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-24 20:02:05 -0700

    update node to send script to

--- airflow/tasks/run_news_batch.sh
+++ airflow/tasks/run_news_batch.sh
@@ -3,6 +3,6 @@ set -eux
 
 . ~/address.sh 
 #TODO: get hostip from address
-ssh -i ~/.ssh/insight-andy.pem ubuntu@$master_pubdns <<- 'ENDSSH'
+ssh -i ~/.ssh/insight-andy.pem ubuntu@$spark1_pubdns <<- 'ENDSSH'
 spark-submit  --master spark://ip-172-31-11-143:7077 --driver-memory 5G --executor-memory 5G ~/insightproject/batch_processing/twitter.py
 ENDSSH
