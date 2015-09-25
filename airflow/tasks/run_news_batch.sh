d1272fb705338a28f43880ce1373bbb421b99a31
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-24 21:34:39 -0700

    set memory for news processing down

--- airflow/tasks/run_news_batch.sh
+++ airflow/tasks/run_news_batch.sh
@@ -4,5 +4,5 @@ set -eux
 . ~/address.sh 
 #TODO: get hostip from address
 ssh -i ~/.ssh/insight-andy.pem ubuntu@$spark1_pubdns <<- 'ENDSSH'
-spark-submit  --master spark://ip-172-31-11-143:7077 --driver-memory 5G --executor-memory 5G ~/insightproject/batch_processing/twitter.py
+spark-submit  --master spark://ip-172-31-11-143:7077 --driver-memory 2G --executor-memory 2G ~/insightproject/batch_processing/twitter.py
 ENDSSH
