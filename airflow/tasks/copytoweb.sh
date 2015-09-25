d7239a909dc8ab5b88bbb9cc039fe6c0ff18ee85
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-24 19:27:19 -0700

    bug fix: update path to scripts

--- airflow/tasks/copytoweb.sh
+++ airflow/tasks/copytoweb.sh
@@ -5,11 +5,11 @@ stream=$1
 . ~/address.sh
 if [ $stream == "rts1" ]; then
 ssh -i ~/.ssh/insight-andy.pem ubuntu@$db1_pubdns <<- 'ENDSSH'
-cqlsh -f ~/insightproject/airflow/copytoweb_rts1.cql
+cqlsh -f ~/insightproject/airflow/tasks/copytoweb_rts1.cql
 ENDSSH
 fi
 if [ $stream == "rts2" ]; then
 ssh -i ~/.ssh/insight-andy.pem ubuntu@$db1_pubdns <<- 'ENDSSH'
-cqlsh -f ~/insightproject/airflow/copytoweb_rts2.cql
+cqlsh -f ~/insightproject/airflow/tasks/copytoweb_rts2.cql
 ENDSSH
 fi
