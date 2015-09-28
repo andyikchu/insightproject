e862bd92f36460c44b44a1b6e2ca8dccd12b5a9c
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-28 12:19:33 -0700

    bug fix: pull tweets continuously in a while true loop

--- inputdata/synthetic_twitter_stream.sh
+++ inputdata/synthetic_twitter_stream.sh
@@ -2,5 +2,7 @@
 set -eu
 
 #every 2 seconds, pull 1 tweet from one company in the historical archives and send it as a kafka message
+while true; do
 python synthetic_twitter_stream.py
 sleep 2
+done
