893c4c721caa6a070a8d5dd7f082b1a7b201aa74
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-28 11:51:51 -0700

    bug fix: put while true around script to keep it running

--- inputdata/twitter_archive.sh
+++ inputdata/twitter_archive.sh
@@ -2,9 +2,10 @@
 set -eu
 
 #Poll twitter to generate a historical set of tweets for later synthetic tweet generation
-
+while true; do
 python twitter_search.py
 for file in /home/ubuntu/synthetic_twitter/*.txt ; do
 	sort -u $file >> $file.archive
 done
 sleep 10m
+done
