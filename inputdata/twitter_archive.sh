b082bc7cbc55b04ba3a0a695bcfc2c3bd4ff5bfe
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-28 12:19:01 -0700

    use correct path to python script

--- inputdata/twitter_archive.sh
+++ inputdata/twitter_archive.sh
@@ -2,8 +2,9 @@
 set -eu
 
 #Poll twitter to generate a historical set of tweets for later synthetic tweet generation
+dir=`dirname $0`
 while true; do
-python twitter_search.py
+python $dir/twitter_search.py
 for file in /home/ubuntu/synthetic_twitter/*.txt ; do
 	sort -u $file >> $file.archive
 done
