8b551bbc09402afa804ba023038763420070538a
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-28 18:37:44 -0700

    replace batch processing of twitter by setting TTL in cassandra insert

--- airflow/tasks/run_news_batch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-
-. ~/address.sh 
-#TODO: get hostip from address
-ssh -i ~/.ssh/insight-andy.pem ubuntu@$spark1_pubdns <<- 'ENDSSH'
-spark-submit  --master spark://ip-172-31-11-143:7077 --driver-memory 2G --executor-memory 2G ~/insightproject/batch_processing/twitter.py
-ENDSSH
