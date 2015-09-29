2f37c750aba3fed6ae97d7fcc8805e4cba276496
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-28 20:05:03 -0700

    replace batch processing of twitter by setting TTL in cassandra insert

--- airflow/financial_news.py
+++ airflow/financial_news.py
@@ -20,148 +20,130 @@ dag = DAG(
     'test', default_args=default_args, schedule_interval=timedelta(1))
 
 #Run Camus to pull messages from Kafka into HDFS
-task1_camus = BashOperator(
-        task_id = '01_camus',
+camus_a = BashOperator(
+        task_id = 'camus_a',
         bash_command='bash tasks/run_camus.sh',
         depends_on_past=1,
         dag = dag)
 
-#Run Spark to pull latest relevant news from HDFS to Cassandra
-task2_news_batch = BashOperator(
-        task_id = '02_news_batch',
-        bash_command='bash tasks/run_news_batch.sh',
-        depends_on_past=1,
-        dag = dag)
-
-task2_news_batch.set_upstream(task1_camus)
-
 #Run Spark to sum all historical trades and write to Cassandra
-task3_trades_batch = BashOperator(
-        task_id = '03_trades_batch',
+trades_batch_a = BashOperator(
+        task_id = 'trades_batch_a',
         bash_command='bash tasks/run_trades_batch.sh',
         depends_on_past=1,
         dag = dag)
 
 #set trades batch after news batch to give it more memory
-task3_trades_batch.set_upstream(task2_news_batch)
+trades_batch_a.set_upstream(camus_a)
 
 #stop streaming of trades while the database is getting updated
-task4_stop_trade_stream = BashOperator(
-        task_id = '04_stop_trade_stream',
+stop_trade_stream_a = BashOperator(
+        task_id = 'stop_trade_stream_a',
         bash_command='bash tasks/stop_trade_stream.sh',
         depends_on_past=1,
         dag = dag)
 
-task4_stop_trade_stream.set_upstream(task3_trades_batch)
+stop_trade_stream_a.set_upstream(trades_batch_a)
 
 #Update Cassandra's stream 2 table to include counts from the batch run with all the trades summed from stock_count_rts1, which were the trades that came in since task1_camus started running
-task5_sum_batch_rts2 = BashOperator(
-        task_id = '05_batch_rts2',
+sum_batch_a_rts2 = BashOperator(
+        task_id = 'sum_batch_a_rts2',
         bash_command='bash tasks/sum_batch_rts rts2',
         depends_on_past=1,
         dag = dag)
 
-task5_sum_batch_rts2.set_upstream(task4_stop_trade_stream)
+sum_batch_a_rts2.set_upstream(stop_trade_stream_a)
 
 #Empty Cassandra's stock_count_rts1 table to set initial counts to the result of the batch calculation
-task6_initialize_db_rts1 = BashOperator(
-        task_id = '06_initialize_rts1',
+initialize_db_a_rts1 = BashOperator(
+        task_id = 'initialize_db_a_rts1',
         bash_command='bash tasks/truncate_rts.sh rts1',
         depends_on_past=1,
         dag = dag)
 
-task6_initialize_db_rts1.set_upstream(task4_stop_trade_stream)
+initialize_db_a_rts1.set_upstream(stop_trade_stream_a)
 
 #Get the web interface to start reading from the newly batch updated rts2
-task7_swap_web_db_rts2 =  BashOperator(
-        task_id = '07_web_rts2',
+swap_web_db_a_rts2 =  BashOperator(
+        task_id = 'swap_web_db_a_rts2',
         bash_command='bash tasks/switchwebdb.sh rts2',
         depends_on_past=1,
         dag = dag)
 
-task7_swap_web_db_rts2.set_upstream(task5_sum_batch_rts2)
+swap_web_db_a_rts2.set_upstream(sum_batch_a_rts2)
 
 #start the trades stream back up
-task8_start_trade_stream = BashOperator(
-        task_id = '08_start_trade_stream',
+start_trade_stream_a = BashOperator(
+        task_id = 'start_trade_stream_a',
         bash_command='bash tasks/start_trade_stream.sh',
         depends_on_past=1,
         dag = dag)
 
-task8_start_trade_stream.set_upstream(task6_initialize_db_rts1)
-task8_start_trade_stream.set_upstream(task7_swap_web_db_rts2)
+start_trade_stream_a.set_upstream(initialize_db_a_rts1)
+start_trade_stream_a.set_upstream(swap_web_db_a_rts2)
 
-#Start next batch run while stock_count_rts1 tracks trades occuring since the completion of task3, the last batch calculation
+#Start next batch run while stock_count_rts1 tracks trades occuring since the completion of task2, the last batch calculation
 #Run Camus to pull messages from Kafka into HDFS
-task9_camus = BashOperator(
-        task_id = '09_camus',
+camus_b = BashOperator(
+        task_id = 'camus_b',
         bash_command='bash tasks/run_camus.sh',
         depends_on_past=1,
         dag = dag)
 
-task9_camus.set_upstream(task8_start_trade_stream)
-
-#Run Spark to pull latest relevant news from HDFS to Cassandra
-task10_news_batch = BashOperator(
-        task_id = '10_news_batch',
-        bash_command='bash tasks/run_news_batch.sh',
-        depends_on_past=1,
-        dag = dag)
-
-task10_news_batch.set_upstream(task9_camus)
+camus_b.set_upstream(start_trade_stream_a)
 
 #Run Spark to sum all historical trades and write to Cassandra
-task11_trades_batch = BashOperator(
-        task_id = '11_trades_batch',
+trades_batch_b = BashOperator(
+        task_id = 'trades_batch_b',
         bash_command='bash tasks/run_trades_batch.sh',
         depends_on_past=1,
         dag = dag)
 
 #set trades batch after news batch to give it more memory
-task11_trades_batch.set_upstream(task10_news_batch)
+trades_batch_b.set_upstream(camus_b)
 
 #stop streaming of trades while the database is getting updated
-task12_stop_trade_stream = BashOperator(
-        task_id = '12_stop_trade_stream',
+stop_trade_stream_b = BashOperator(
+        task_id = 'stop_trade_stream_b',
         bash_command='bash tasks/stop_trade_stream.sh',
         depends_on_past=1,
         dag = dag)
 
-task12_stop_trade_stream.set_upstream(task11_trades_batch)
+stop_trade_stream_b.set_upstream(trades_batch_b)
 
-#Update Cassandra's stream 1 table to include counts from the batch run with all the trades summed from stock_count_rts2, which were the trades that came in since task9_camus started running
-task13_sum_batch_rts1 = BashOperator(
-        task_id = '13_batch_rts1',
+#Update Cassandra's stream 1 table to include counts from the batch run with all the trades summed from stock_count_rts2, which were the trades that came in since task8_camus started running
+sum_batch_b_rts1 = BashOperator(
+        task_id = 'sum_batch_b_rts1',
         bash_command='bash tasks/sum_batch_rts rts1',
         depends_on_past=1,
         dag = dag)
 
-task13_sum_batch_rts1.set_upstream(task12_stop_trade_stream)
+sum_batch_b_rts1.set_upstream(stop_trade_stream_b)
 
 #Empty Cassandra's stock_count_rts2 table to set initial counts to the result of the batch calculation
-task14_initialize_db_rts2 = BashOperator(
-        task_id = '14_initialize_rts2',
+initialize_db_b_rts2 = BashOperator(
+        task_id = 'initialize_db_b_rts2',
         bash_command='bash tasks/truncate_rts.sh rts2',
         depends_on_past=1,
         dag = dag)
 
-task14_initialize_db_rts2.set_upstream(task12_stop_trade_stream)
+initialize_db_b_rts2.set_upstream(stop_trade_stream_b)
 
 #Get the web interface to start reading from the newly batch updated rts1
-task15_swap_web_db_rts1 =  BashOperator(
-        task_id = '15_web_rts1',
+swap_web_db_b_rts1 =  BashOperator(
+        task_id = 'swap_web_db_b_rts1',
         bash_command='bash tasks/switchwebdb.sh rts1',
         depends_on_past=1,
         dag = dag)
 
-task15_swap_web_db_rts1.set_upstream(task13_sum_batch_rts1)
+swap_web_db_b_rts1.set_upstream(sum_batch_b_rts1)
 
 #start the trades stream back up
-task16_start_trade_stream = BashOperator(
-        task_id = '16_start_trade_stream',
+start_trade_stream_b = BashOperator(
+        task_id = 'start_trade_stream_b',
         bash_command='bash tasks/start_trade_stream.sh',
         depends_on_past=1,
         dag = dag)
 
-task16_start_trade_stream.set_upstream(task14_initialize_db_rts2)
-task16_start_trade_stream.set_upstream(task15_swap_web_db_rts1)
+start_trade_stream_b.set_upstream(initialize_db_b_rts2)
+start_trade_stream_b.set_upstream(swap_web_db_b_rts1)
