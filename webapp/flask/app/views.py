3c6dc1a206c852c724f88fcc9bff9fda5937d929
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-30 13:15:38 -0700

    first attempt to put database content onto frontend

--- webapp/flask/app/views.py
+++ webapp/flask/app/views.py
@@ -14,8 +14,18 @@ def index():
 
 @app.route('/user')
 def get_user():
+    #temporary hacky method to query for all companies; TODO: redesign cassandra schema or use Presto to do this 
+    COMPANIES = ["MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DD", "XOM", "GE", "GS", "HD", "INTC", "IBM", "JNJ", "JPM", "MCD", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV", "UNH", "UTX", "VZ", "V", "WMT", "DIS"]
     user=request.args.get("user")
-    return render_template("user.html", user="2")
+
+    #check which database to query
+    dbfile = open("/home/ubuntu/.insightproject/cassandra.txt")
+    db = dbfile.readline().rstrip()
+    dbfile.close()
+
+    latest_trades = session.execute("SELECT company, numstock, tradetime FROM trade_history WHERE user=%s AND company IN (%s) ORDER BY tradetime LIMIT 5", parameters=[user, ','.join(COMPANIES)])
+
+    return render_template("user.html", user=user, latest_trades = latest_trades)
 
 @app.route('/tradesummary/<user>')
 def get_trade_summary(user):
