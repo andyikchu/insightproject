fa06109dd3de0bd65b08f435dc829e8799cbcf54
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-30 17:11:57 -0700

    correct ssh dns addresses

--- airflow/tasks/start_trade_stream.sh
+++ airflow/tasks/start_trade_stream.sh
@@ -3,4 +3,4 @@ set -eu
 
 . ~/address.sh
 #TODO: read hostname from address.sh
-ssh -i ~/.ssh/insight-andy.pem ubuntu@spark1 -t 'tmux new-session -d -s trade_stream ". ~/address.sh; . ~/.profile ; spark-submit --master spark://ip-172-31-11-143:7077 --driver-memory 2G --executor-memory 2G --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.1 ~/insightproject/realtime_processing/trades_stream.py"'
+ssh -i ~/.ssh/insight-andy.pem ubuntu@$spark1_pubdns -t 'tmux new-session -d -s trade_stream ". ~/address.sh; . ~/.profile ; spark-submit --master spark://ip-172-31-11-143:7077 --driver-memory 2G --executor-memory 2G --packages org.apache.spark:spark-streaming-kafka_2.10:1.5.1 ~/insightproject/realtime_processing/trades_stream.py"'
--- airflow/tasks/stop_trade_stream.sh
+++ airflow/tasks/stop_trade_stream.sh
@@ -2,6 +2,6 @@
 set -eu
 
 . ~/address.sh
-ssh -i .ssh/insight-andy.pem ubuntu@spark1 <<- 'ENDSSH'
+ssh -i .ssh/insight-andy.pem ubuntu@$spark1_pubdns <<- 'ENDSSH'
 tmux kill-session -t trade_stream
 ENDSSH
