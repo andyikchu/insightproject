ab0cd58c9e650dcca9aa37a12dda0736ee3e660d
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-09-30 17:13:56 -0700

    increase memory

--- airflow/tasks/sum_batch_rts.sh
+++ airflow/tasks/sum_batch_rts.sh
@@ -7,11 +7,11 @@ stream=$1
 . ~/address.sh
 if [ $stream == "rts1" ]; then
 ssh -i ~/.ssh/insight-andy.pem ubuntu@$spark1_pubdns <<- 'ENDSSH'
-spark-submit --master spark://ip-172-31-11-143:7077 --driver-memory 5G --executor-memory 5G ~/insightproject/airflow/tasks/sum_batch_rts.py rts1
+spark-submit --master spark://ip-172-31-11-143:7077 --driver-memory 10G --executor-memory 10G ~/insightproject/airflow/tasks/sum_batch_rts.py rts1
 ENDSSH
 fi
 if [ $stream == "rts2" ]; then
 ssh -i ~/.ssh/insight-andy.pem ubuntu@$spark1_pubdns <<- 'ENDSSH'
-spark-submit --master spark://ip-172-31-11-143:7077 --driver-memory 5G --executor-memory 5G ~/insightproject/airflow/tasks/sum_batch_rts.py rts2
+spark-submit --master spark://ip-172-31-11-143:7077 --driver-memory 10G --executor-memory 10G ~/insightproject/airflow/tasks/sum_batch_rts.py rts2
 ENDSSH
 fi
