5f69647745e0914b6e1810fa86185cc57c6fcb79
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-10-02 15:09:25 -0700

    make the supertraders trade 2 stocks every second

--- inputdata/trades_superusers.R
+++ inputdata/trades_superusers.R
@@ -1,13 +1,13 @@
-#10 users constantly trade every stock
+#10 users trade 2 companies every second
 traderIDs = seq(1,10)
 tickerIDs = c("MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DD", "XOM", "GE", "GS", "HD", "INTC", "IBM", "JNJ", "JPM", "MCD", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV", "UNH", "UTX", "VZ", "V", "WMT", "DIS")
 
 library(rkafka)
 
 generate_trades = function() {
-	trades = data.frame(trader=sort(rep(traderIDs, length(tickerIDs))))
+	trades = data.frame(trader=sort(rep(traderIDs, 2)))
 	#associate a ticker name to trade from every trader
-	trades["ticker"] = rep(tickerIDs, length(traderIDs))
+	trades["ticker"] = sample(tickerIDs, nrow(trades), replace=T)
 	#associate a quantity of stock to buy or sell for each trade
 	trades["numstocks"] = floor(rnorm(nrow(trades), mean=10, sd=750))
 	
@@ -23,4 +23,5 @@ generate_trades = function() {
 
 while(1) {
 	generate_trades()
+	sleep(1)
 }
