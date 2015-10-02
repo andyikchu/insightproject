8e177f34a2bca19a1315ad048cc7d28bfd598772
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-10-02 11:43:08 -0700

    add a producer that focuses on a small set of traders to pump out a lot of trades for quick demos

---
+++ inputdata/trades_superusers.R
@@ -0,0 +1,26 @@
+#10 users constantly trade every stock
+traderIDs = seq(1,10)
+tickerIDs = c("MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DD", "XOM", "GE", "GS", "HD", "INTC", "IBM", "JNJ", "JPM", "MCD", "MRK", "MSFT", "NKE", "PFE", "PG", "TRV", "UNH", "UTX", "VZ", "V", "WMT", "DIS")
+
+library(rkafka)
+
+generate_trades = function() {
+	trades = data.frame(trader=sort(rep(traderIDs, length(tickerIDs))))
+	#associate a ticker name to trade from every trader
+	trades["ticker"] = rep(tickerIDs, length(traderIDs))
+	#associate a quantity of stock to buy or sell for each trade
+	trades["numstocks"] = floor(rnorm(nrow(trades), mean=10, sd=750))
+	
+	kafkanode="ec2-54-215-247-116.us-west-1.compute.amazonaws.com:9092"
+	producer=rkafka.createProducer(kafkanode)
+
+	for(i in 1:nrow(trades)) {
+		message = paste0('{"user":', trades[i,"trader"], ', "company":"', trades[i,"ticker"],'", "numstock":', trades[i,"numstocks"], ', "timestamp":"', Sys.time(), '"}')
+		rkafka.send(producer, "trades", kafkanode, message)
+	}
+	rkafka.closeProducer(producer)
+}
+
+while(1) {
+	generate_trades()
+}
