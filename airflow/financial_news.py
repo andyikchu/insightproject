1d7edb08cda1bf257aec64acbf1f93c9816d6a3b
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-10-03 15:44:21 -0700

    logic fix: don't stop reading in trades stream until summation completes (it can be assumed to be part of batch processing)

--- airflow/financial_news.py
+++ airflow/financial_news.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'ubuntu',
     'depends_on_past': False,
-    'start_date': datetime(2015, 9, 26),
+    'start_date': datetime(2015, 10, 2),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -36,15 +36,6 @@ trades_batch_a = BashOperator(
 #set trades batch after news batch to give it more memory
 trades_batch_a.set_upstream(camus_a)
 
-#stop streaming of trades while the database is getting updated
-stop_trade_stream_a = BashOperator(
-        task_id = 'stop_trade_stream_a',
-        bash_command='tasks/stop_trade_stream.sh',
-        depends_on_past=1,
-        dag = dag)
-
-stop_trade_stream_a.set_upstream(trades_batch_a)
-
 #Update Cassandra's stream 2 table to include counts from the batch run with all the trades summed from stock_count_rts1, which were the trades that came in since task1_camus started running
 sum_batch_a_rts2 = BashOperator(
         task_id = 'sum_batch_a_rts2',
@@ -52,7 +43,17 @@ sum_batch_a_rts2 = BashOperator(
         depends_on_past=1,
         dag = dag)
 
-sum_batch_a_rts2.set_upstream(stop_trade_stream_a)
+sum_batch_a_rts2.set_upstream(trades_batch_a)
+
+#stop streaming of trades while the database is getting updated
+stop_trade_stream_a = BashOperator(
+        task_id = 'stop_trade_stream_a',
+        bash_command='tasks/stop_trade_stream.sh',
+        depends_on_past=1,
+        dag = dag)
+
+stop_trade_stream_a.set_upstream(sum_batch_a_rts2)
+
 
 #Empty Cassandra's stock_count_rts1 table to set initial counts to the result of the batch calculation
 initialize_db_a_rts1 = BashOperator(
@@ -70,7 +71,7 @@ swap_web_db_a_rts2 =  BashOperator(
         depends_on_past=1,
         dag = dag)
 
-swap_web_db_a_rts2.set_upstream(sum_batch_a_rts2)
+swap_web_db_a_rts2.set_upstream(stop_trade_stream_a)
 
 #start the trades stream back up
 start_trade_stream_a = BashOperator(
@@ -102,15 +103,6 @@ trades_batch_b = BashOperator(
 #set trades batch after news batch to give it more memory
 trades_batch_b.set_upstream(camus_b)
 
-#stop streaming of trades while the database is getting updated
-stop_trade_stream_b = BashOperator(
-        task_id = 'stop_trade_stream_b',
-        bash_command='tasks/stop_trade_stream.sh',
-        depends_on_past=1,
-        dag = dag)
-
-stop_trade_stream_b.set_upstream(trades_batch_b)
-
 #Update Cassandra's stream 1 table to include counts from the batch run with all the trades summed from stock_count_rts2, which were the trades that came in since task8_camus started running
 sum_batch_b_rts1 = BashOperator(
         task_id = 'sum_batch_b_rts1',
@@ -118,7 +110,16 @@ sum_batch_b_rts1 = BashOperator(
         depends_on_past=1,
         dag = dag)
 
-sum_batch_b_rts1.set_upstream(stop_trade_stream_b)
+sum_batch_b_rts1.set_upstream(trades_batch_b)
+
+#stop streaming of trades while the database is getting updated
+stop_trade_stream_b = BashOperator(
+        task_id = 'stop_trade_stream_b',
+        bash_command='tasks/stop_trade_stream.sh',
+        depends_on_past=1,
+        dag = dag)
+
+stop_trade_stream_b.set_upstream(sum_batch_b_rts1)
 
 #Empty Cassandra's stock_count_rts2 table to set initial counts to the result of the batch calculation
 initialize_db_b_rts2 = BashOperator(
@@ -136,7 +137,7 @@ swap_web_db_b_rts1 =  BashOperator(
         depends_on_past=1,
         dag = dag)
 
-swap_web_db_b_rts1.set_upstream(sum_batch_b_rts1)
+swap_web_db_b_rts1.set_upstream(stop_trade_stream_b)
 
 #start the trades stream back up
 start_trade_stream_b = BashOperator(
