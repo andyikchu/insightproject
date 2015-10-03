ce63e12cd6c4200e35a2bc78f2dc381d6b7f5923
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-10-03 16:58:39 -0700

    set hard path to spark-submit instead of relying on environment

--- airflow/tasks/run_trades_batch.sh
+++ airflow/tasks/run_trades_batch.sh
@@ -4,5 +4,5 @@ set -eu
 . ~/address.sh 
 dir=`dirname $0`
 for file in `bash $dir/hdfs-ls.sh 2>&1 | grep camus`; do
-ssh -i ~/.ssh/insight-andy.pem ubuntu@$spark1_pubdns "spark-submit --master spark://ip-172-31-11-143:7077 --driver-memory 25G --executor-memory 25G ~/insightproject/batch_processing/trades.py $file"
+ssh -i ~/.ssh/insight-andy.pem ubuntu@$spark1_pubdns "/usr/local/spark/bin/spark-submit --master spark://ip-172-31-11-143:7077 --driver-memory 25G --executor-memory 25G ~/insightproject/batch_processing/trades.py $file"
 done
