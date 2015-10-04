95d588cd242087d19d20aa020ed414d7360bb315
andy.ik.chu@gmail.com <andy.ik.chu@gmail.com>
2015-10-03 17:25:54 -0700

    bug fix, compare percentages between ratio and contact limit; set custom lowered limit for super-traders for demo

--- webapp/flask/app/views.py
+++ webapp/flask/app/views.py
@@ -32,11 +32,14 @@ def _get_user_data(user):
     user_companies_list = []
     #calculate portfolio ratios and generate list of companies above contact limit
     for row in user_companies:
+        #custom limit for super-traders used in the demo
+        if user <= 10 then:
+            row["contact_limit"] = .05
         row["portfolio_ratio"] = 100*float(row["stock_total"])/portfolio_total
+        if row["portfolio_ratio"] > 100 * row["contact_limit"]:
+            user_companies_list.append(row["company"])
         row["graphic"] = '|' * abs(int(round(row["portfolio_ratio"])))
         row["portfolio_ratio"] = "%.2f" % row["portfolio_ratio"]
-        if row["portfolio_ratio"] > row["contact_limit"]:
-            user_companies_list.append(row["company"])
 
     user_companies.sort(key = lambda row: abs(float(row["portfolio_ratio"])), reverse=True)
     latest_news = session.execute("SELECT company, summary, newsoutlet, source, author, newstime FROM news WHERE company IN ('" + "','".join(user_companies_list) + "') ORDER BY newstime DESC LIMIT 10")
